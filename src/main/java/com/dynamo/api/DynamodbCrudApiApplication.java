4a821911cf1cc77ae65edac4b345c1f8104b63f7
riakashyapperfaware <ria_kashyap@perfaware.com>
2026-06-24 08:37:45 -0400

    chore: add project structure and main application class

---
+++ src/main/java/com/dynamo/api/DynamodbCrudApiApplication.java
@@ -0,0 +1,12 @@
+package com.dynamo.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamodbCrudApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DynamodbCrudApiApplication.class, args);
+    }
+}
